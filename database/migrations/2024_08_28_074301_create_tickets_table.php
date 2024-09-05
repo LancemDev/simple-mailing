e81d5f391c647bd1e9f44dc7c460acab29b8ed99
LancemDev <munyaolance1@gmail.com>
2024-09-05 07:52:48 +0300

    :twisted_rightwards_arrows: Fix: Data Types

--- database/migrations/2024_08_28_074301_create_tickets_table.php
+++ database/migrations/2024_08_28_074301_create_tickets_table.php
@@ -19,29 +19,29 @@ return new class extends Migration
             $table->string('participant_first_name');
             $table->string('category')->nullable();
             $table->string('subcategory')->nullable();
-            $table->decimal('price', 8, 2);
+            $table->string('price'); // Changed to string
             $table->string('barcode')->unique();
             $table->boolean('composed')->default(0);
-            $table->date('order_date');
-            $table->time('order_time');
-            $table->date('payment_date');
-            $table->time('payment_time');
+            $table->string('order_date');
+            $table->string('order_time');
+            $table->string('payment_date');
+            $table->string('payment_time');
             $table->string('order_number')->unique();
             $table->string('advanced_order_number')->nullable();
             $table->string('ticket_number')->unique();
             $table->string('currency');
             $table->boolean('deleted')->default(0);
             $table->string('source');
-            $table->decimal('public_price', 8, 2);
-            $table->decimal('total_fees', 8, 2);
+            $table->string('public_price'); // Changed to string
+            $table->string('total_fees'); // Changed to string
             $table->string('discount_code')->nullable();
-            $table->decimal('discount', 8, 2);
-            $table->decimal('total_price_paid_incl_tax', 8, 2);
-            $table->decimal('commission', 8, 2);
-            $table->decimal('total_price_without_commission_incl_tax', 8, 2);
-            $table->decimal('price_paid_excl_tax', 8, 2);
-            $table->decimal('tax_rate', 5, 2);
-            $table->decimal('tax_for_tax_declarations', 8, 2);
+            $table->string('discount'); // Changed to string
+            $table->string('total_price_paid_incl_tax'); // Changed to string
+            $table->string('commission'); // Changed to string
+            $table->string('total_price_without_commission_incl_tax'); // Changed to string
+            $table->string('price_paid_excl_tax'); // Changed to string
+            $table->string('tax_rate'); // Changed to string
+            $table->string('tax_for_tax_declarations'); // Changed to string
             $table->string('counter');
             $table->string('counter_seller');
             $table->string('counter_payment');
@@ -51,10 +51,10 @@ return new class extends Migration
             $table->string('buyer_first_name');
             $table->string('buyer_email')->unique();
             $table->string('ticket_status');
-            $table->timestamp('refund_request_date_and_time')->nullable();
-            $table->timestamp('ticket_deletion_date_and_time')->nullable();
+            $table->string('refund_request_date_and_time')->nullable();
+            $table->string('ticket_deletion_date_and_time')->nullable();
             $table->string('refund_request_source')->nullable();
-            $table->decimal('refund_request_amount', 8, 2)->nullable();
+            $table->string('refund_request_amount')->nullable(); // Changed to string
             $table->string('buyer_mobile')->nullable();
             $table->string('buyer_country');
             $table->string('buyer_company')->nullable();
